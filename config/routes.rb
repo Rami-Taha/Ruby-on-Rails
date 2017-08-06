d40cb41034c46ebf35f1862c548ee236fac5be94
ramiGA <ramisn3@gmail.com>
2017-08-06 18:22:39 -0400

    articlesCRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-   devise_for :users, :controllers => { registrations: 'registrations' }
-=======
-  resources :articles
->>>>>>> articles
-root to: 'pages#index'
+
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
+   resources :articles
+  root to: 'pages#index'
   get 'pages/about'
   get 'pages/contact'
 
